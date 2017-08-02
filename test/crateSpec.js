95dd602637549349130b0fedc8249db4c36bbd20
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-08-02 18:13:04 +0100

    Crates first test

--- test/crateSpec.js
+++ test/crateSpec.js
@@ -5,16 +5,16 @@ describe('Crate', function() {
   describe('crate', function() {
     it('should be given a postion and returns that position', function() {
       var crate = new box.Crate()
-      crate.position(2,1)
-      assert.equal(crate.x, 2)
+      crate.position(9,1)
+      assert.equal(crate.x, 9)
       assert.equal(crate.y, 1)
     });
 
-    // it('should return the correct cordinates of positive', function() {
-    //   var belt = new sim.Belt()
-    //   belt.position(4,2)
-    //   assert.equal(belt.x, 4)
-    //   assert.equal(belt.y, 2)
-    // });
+    it('should return the correct cordinates of positive', function() {
+      var belt = new sim.Belt()
+      belt.position(8,3)
+      assert.equal(belt.x, 8)
+      assert.equal(belt.y, 3)
+    });
   });
 });
