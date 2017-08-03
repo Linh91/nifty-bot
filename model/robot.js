6065d3076e6e68a74388ce395190516b99eceed1
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-08-03 21:41:39 +0100

    Instructions can be in array or string

--- model/robot.js
+++ model/robot.js
@@ -6,6 +6,8 @@ const Robot = function() {
   this.y = 0
   this.belt = new Belt()
   this.crate = new Crate()
+  this.instructions = []
+  this.passedInstruct = []
 }
 
 Robot.prototype.position = function (x, y) {
@@ -13,7 +15,15 @@ Robot.prototype.position = function (x, y) {
   this.y = y
 };
 Robot.prototype.instruct = function(arguments) {
-  return(arguments.split(''))
+  if (typeof arguments == 'string') {
+    arguments = arguments.split('')
+    this.instructions = this.instructions.concat(arguments)
+  } else {
+    this.instructions = this.instructions.concat(arguments)
+  }
 };
 
+Robot.prototype.newPosition = function() {
+
+};
 module.exports = Robot;
--- test/robotSpec.js
+++ test/robotSpec.js
@@ -22,48 +22,56 @@ describe('Robot', function() {
   describe('instructions', function() {
     it('are given for the robot', function() {
       var robot = new Robot();
-      assert.deepEqual(robot.instruct("ns"), ["n", "s"]);
+      robot.instruct("ns")
+      assert.deepEqual(robot.instructions, ["n", "s"]);
     });
 
     it('can be any number of characters', function() {
       var robot = new Robot();
-      assert.deepEqual(robot.instruct("nppsse"), ["n", "p", "p", "s", "s", "e"]);
+      robot.instruct("nppsse")
+      assert.deepEqual(robot.instructions, ["n", "p", "p", "s", "s", "e"]);
     });
 
     it('can be unlimited', function() {
       var robot = new Robot();
-      assert.deepEqual(robot.instruct("newpsesennppppeeee"), ["n", "e", "w", "p", "s", "e","s", "e","n", "n", "p", "p", "p", "p", "e","e","e","e",]);
+      robot.instruct("newpsesennppppeeee")
+      assert.deepEqual(robot.instructions, ["n", "e", "w", "p", "s", "e","s", "e","n", "n", "p", "p", "p", "p", "e","e","e","e",]);
     });
 
-    describe('belt', function() {
-      it('should be able to give belt a position', function() {
-        var robot = new Robot();
-        robot.belt.position(2, 5);
-        assert.equal(robot.belt.x, 2)
-        assert.equal(robot.belt.y, 5)
-      });
+    it('can take directions in an array form', function() {
+      var robot = new Robot();
+      robot.instruct(["s", "n", "e"])
+      assert.deepEqual(robot.instructions, ["s", "n", "e"]);
+    });
+  });
 
-      it('should return x, y cordinates', function() {
-        var robot = new Robot();
-        robot.belt.position(8, 1);
-        assert.equal(robot.belt.x, 8)
-        assert.equal(robot.belt.y, 1)
-      });
+  describe('belt', function() {
+    it('should be able to give belt a position', function() {
+      var robot = new Robot();
+      robot.belt.position(2, 5);
+      assert.equal(robot.belt.x, 2)
+      assert.equal(robot.belt.y, 5)
     });
 
-    describe('crate', function() {
-      it('should be able to give information about crate', function() {
-        var robot = new Robot();
-        robot.crate.description(2, 5, 2);
-        assert.deepEqual(robot.crate.des, [ [ 2, 5, 2 ] ])
-      });
+    it('should return x, y cordinates', function() {
+      var robot = new Robot();
+      robot.belt.position(8, 1);
+      assert.equal(robot.belt.x, 8)
+      assert.equal(robot.belt.y, 1)
+    });
+  });
 
-      // it('should return x, y cordinates', function() {
-      //   var robot = new Robot();
-      //   robot.belt.position(8, 1);
-      //   assert.equal(robot.belt.x, 8)
-      //   assert.equal(robot.belt.y, 1)
-      // });
+  describe('crate', function() {
+    it('should be able to give information about crate', function() {
+      var robot = new Robot();
+      robot.crate.description(2, 5, 2);
+      assert.deepEqual(robot.crate.des, [ [ 2, 5, 2 ] ]);
+    });
+
+    it('will return details about crate', function() {
+      var robot = new Robot();
+      robot.crate.description(4, 1, 6);
+      assert.deepEqual(robot.crate.des, [ [ 4, 1, 6 ] ])
     });
   });
 });
