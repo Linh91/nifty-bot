3ce35821165911b1c0040a074d04bc79f3a787c5
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-08-03 23:25:21 +0100

    Crate spec now updated

--- test/crateSpec.js
+++ test/crateSpec.js
@@ -6,13 +6,17 @@ describe('Crate', function() {
     it('should be given a postion and returns that position', function() {
       var crate = new Crate()
       crate.description(9, 1, 5)
-      assert.deepEqual(crate.des, [ [ 9, 1, 5 ] ])
+      assert.equal(crate.x, 9)
+      assert.equal(crate.y, 1)
+      assert.equal(crate.bags, 5)
     });
 
     it('should return an array of three elements', function() {
       var crate = new Crate()
       crate.description(8, 3, 8)
-      assert.deepEqual(crate.des, [ [ 8, 3, 8 ] ])
+      assert.equal(crate.x, 8)
+      assert.equal(crate.y, 3)
+      assert.equal(crate.bags, 8)
     });
   });
 });
