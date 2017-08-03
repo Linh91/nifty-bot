a3d22bee9c0d1078479f089dcd57678a7da492c9
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-08-03 20:13:08 +0100

    Added belt test for robot spec

--- test/robotSpec.js
+++ test/robotSpec.js
@@ -1,5 +1,6 @@
 const Robot = require("../model/robot")
 var assert = require('assert');
+var expect = require('chai').expect;
 
 describe('Robot', function() {
   describe('position', function() {
@@ -20,18 +21,34 @@ describe('Robot', function() {
 
   describe('instructions', function() {
     it('are given for the robot', function() {
-      var robot = new Robot()
-      assert.deepEqual(robot.instruct("ns"), ["n", "s"])
+      var robot = new Robot();
+      assert.deepEqual(robot.instruct("ns"), ["n", "s"]);
     });
 
     it('can be any number of characters', function() {
-      var robot = new Robot()
-      assert.deepEqual(robot.instruct("nppsse"), ["n", "p", "p", "s", "s", "e"])
+      var robot = new Robot();
+      assert.deepEqual(robot.instruct("nppsse"), ["n", "p", "p", "s", "s", "e"]);
     });
 
     it('can be unlimited', function() {
-      var robot = new Robot()
-      assert.deepEqual(robot.instruct("newpsesennppppeeee"), ["n", "e", "w", "p", "s", "e","s", "e","n", "n", "p", "p", "p", "p", "e","e","e","e",])
+      var robot = new Robot();
+      assert.deepEqual(robot.instruct("newpsesennppppeeee"), ["n", "e", "w", "p", "s", "e","s", "e","n", "n", "p", "p", "p", "p", "e","e","e","e",]);
+    });
+
+    describe('belt', function() {
+      it('should be able to give belt a position', function() {
+        var robot = new Robot();
+        robot.belt.position(2, 5);
+        assert.equal(robot.belt.x, 2)
+        assert.equal(robot.belt.y, 5)
+      });
+
+      it('should return x, y cordinates', function() {
+        var robot = new Robot();
+        robot.belt.position(8, 1);
+        assert.equal(robot.belt.x, 8)
+        assert.equal(robot.belt.y, 1)
+      });
     });
   });
 });
