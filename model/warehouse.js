4efe77d5b1a5f17724c1bf757e32b52febecb12e
Ngoc L Ha <sammy_nlh@hotmail.com>
2017-08-04 13:45:29 +0100

    CommandOutput changed to print not broken

--- model/warehouse.js
+++ model/warehouse.js
@@ -20,27 +20,25 @@ Warehouse.prototype.commands = function(arguments) {
 Warehouse.prototype.run = function () {
   if (this.instructions[0] == "n" || this.instructions[0] == "s") this.yAxis();
   else if (this.instructions[0] == "e" || this.instructions[0] == "w") this.xAxis();
-  else if (this.instructions[0] == "d" || this.instructions[0] == "p") return this.bagDuty();
+  else if (this.instructions[0] == "d" || this.instructions[0] == "p") return this.pickDropFunction();
   else this.broken();
 };
 
 Warehouse.prototype.yAxis = function () {
-  (this.instructions[0] == "n") ? this.robot.position[1] += 1 : this.robot.position[1] -= 1
-  this.commandOutput()
+  (this.instructions[0] == "n") ? this.robot.position[1] += 1 : this.robot.position[1] -= 1;
+  this.commandOutput();
 };
 
 Warehouse.prototype.xAxis = function () {
-  (this.instructions[0] == "e") ? this.robot.position[0] += 1 : this.robot.position[0] -= 1
-  this.commandOutput()
+  (this.instructions[0] == "e") ? this.robot.position[0] += 1 : this.robot.position[0] -= 1;
+  this.commandOutput();
 };
 
-Warehouse.prototype.bagDuty = function () {
+Warehouse.prototype.pickDropFunction = function () {
   if (this.instructions[0] == "p") {
     this.cratePos.push(this.crate.cratesArray[0][0])
     this.cratePos.push(this.crate.cratesArray[0][1])
-    if (this.instructions[0] == "p" && this.robot.position.toString() == this.cratePos) {
-      this.pickup();
-    }
+    if (this.instructions[0] == "p" && this.robot.position.toString() == this.cratePos) this.pickup();
   } else if (this.instructions[0] == "d" && this.robot.position.toString() == this.belt.position.toString()) {
     this.droppedBags = this.bags;
     this.commandOutput()
@@ -68,7 +66,7 @@ Warehouse.prototype.pickup = function () {
 
 Warehouse.prototype.commandOutput = function () {
   this.instructions.shift()
-  if (this.instructions.length == 0) return this.broken()
+  if (this.instructions.length == 0) return this.print()
 };
 
 Warehouse.prototype.print = function () {
